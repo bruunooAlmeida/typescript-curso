35f3b217b5a16b16d2a2b89814a5d4cc80d6a0fd
bruunooAlmeida <almeidabrunoti@gmail.com>
2023-06-22 23:12:57 -0300

    Finalizando Curso

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,35 +1,38 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
-export class NegociacaoController{
-    private inputData : HTMLInputElement ;
-    private inputQuantidade: HTMLInputElement;
-    private inputValor : HTMLInputElement;
-    private negociacoes : Negociacao
-    
-    constructor() {
-        this.inputData       = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor      = document.querySelector('#valor');
-    }
+export class NegociacaoController {
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
+  private negociacoes = new Negociacoes();
 
-    adiciona() : void {
-        const negociacao = this.criaNegociacao();
-        console.log(negociacao);                                        
-        this.limparFormulario();
-    } 
+  constructor() {
+    this.inputData = document.querySelector("#data");
+    this.inputQuantidade = document.querySelector("#quantidade");
+    this.inputValor = document.querySelector("#valor");
+  }
 
-    criaNegociacao() : Negociacao{
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ","));
-        const quantidade = parseInt(this.inputValor.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
-    }
+  adiciona(): void {
+    const negociacao = this.criaNegociacao();
+    negociacao.data.setDate(12);
+    this.negociacoes.adiciona(negociacao);
+    console.log(this.negociacoes.lista());
+    this.limparFormulario();
+  }
 
-    limparFormulario() : void{
-        this.inputData.value       = '';
-        this.inputQuantidade.value = '';
-        this.inputValor.value      = '';
-        this.inputData.focus
-    }
+  criaNegociacao(): Negociacao {
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ","));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+    return new Negociacao(date, quantidade, valor);
+  }
+
+  limparFormulario(): void {
+    this.inputData.value = "";
+    this.inputQuantidade.value = "";
+    this.inputValor.value = "";
+    this.inputData.focus();
+  }
 }
